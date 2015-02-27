8084cfe6378e6fc8ad2bbf9f439cdcebe241793a
el-josu3 <josu3.ich@gmail.com>
2015-02-26 20:00:48 -0800

    Tarea3
    Numero primo o no.

---
+++ Tareas/src/Tarea1/Primos.java
@@ -0,0 +1,19 @@
+package Tarea1;
+import java.*;
+import java.util.Scanner;
+
+public class Primos {
+
+	public static void main(String[] args) {
+	int num1=0;
+	System.out.println("INGRESE 1 NUMEROS");
+	Scanner j=new Scanner(System.in);
+	num1=j.nextInt();
+		if(num1%2==0)
+		System.out.println("EL NUMERO INGERSADO ES PAR");
+	else
+		System.out.println("EL NUMERO INGRESADO ES IMPAR");
+	
+	}
+
+}
